a5450e646f8168ca58f5375941ec6a747a4bec48
flashmob1441 <alexahdro33@gmail.com>
2024-10-31 22:05:40 +0500

    Fix pyrogram bug.

--- utils/get_chat_members.py
+++ utils/get_chat_members.py
@@ -1,9 +1,21 @@
 import os
 
-from pyrogram import Client
+from pyrogram import Client, utils
+
+
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
 
 
 async def get_chat_members(chat_id: int) -> list[int]:
+    utils.get_peer_type = get_peer_type_new
+
     members = []
 
     async with Client("caller",
